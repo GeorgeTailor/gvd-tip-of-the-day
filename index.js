f8c3eceff048276d0f955083800acf88491c2c58
Yuriy Kravets <yucikkravets@gmail.com>
2021-07-16 00:57:32 +0200

    set up http server as heroku requires it

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const { Client, Intents, MessageEmbed } = require('discord.js');
+import { Client, Intents, MessageEmbed } from 'discord.js';
+import http from 'http';
+import fs from 'fs';
+const port = process.argv[2] || 9000;
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+http.createServer(function (req, res) {
+	console.log(`${req.method} ${req.url}`);
+
+	const pathname = '/index.html';
+	fs.readFile(pathname, function (err, data) {
+		res.end(data);
+	});
+}).listen(parseInt(port));
+
+console.log(`Server listening on port ${port}`);
 
 const tips = require('./tips.json');
 
