81324884039d27cb39d6566d88cd2a03f656035d
Yuriy Kravets <yucikkravets@gmail.com>
2021-07-15 19:17:33 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const { Client, Intents, MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+const tips = require('./tips.json');
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+	client.channels.cache.get('865252270009352243').send('Ready to serve.');
+
+	runJob();
+});
+
+// https://daily.heroeswm.ru/help/
+
+const runJob = () => {
+	// 865252270009352243 - test
+	// 864982486702227507 - общий-чат
+	const tip = tips[Math.floor(Math.random()*tips.length)];
+		const channel = client.channels.cache.get('865252270009352243');
+		channel.send(`${tip.title}\n${tip.content}`);
+
+		tip?.links.forEach(link => {
+			const embed = new MessageEmbed()
+				.setTitle(link.title)
+				.setColor(0xff0000)
+				.setDescription(link.description);
+			channel.send(embed);
+		});
+	setTimeout(() => {
+		runJob();
+	}, 86_400_000); // once per day
+}
+
+client.on('message', message => {
+	if (message.content === '!tip') {
+		const tip = tips[Math.floor(Math.random()*tips.length)];
+		const channel = client.channels.cache.get('865252270009352243');
+		channel.send(`${tip.title}\n${tip.content}`);
+
+		tip?.links.forEach(link => {
+			const embed = new MessageEmbed()
+				.setTitle(link.title)
+				.setColor(0xff0000)
+				.setDescription(link.description);
+			channel.send(embed);
+		});
+	}
+});
+
+client.login(process.env.BOT_TOKEN);
